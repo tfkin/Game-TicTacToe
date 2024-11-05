4526daaadc637330b67d2d70aaaf2a2e5aeb3d0c
Kin <Kfurigay4119@conestogac.on.ca>
2024-11-04 21:26:55 -0500

    Add project files.

---
+++ KFurigayAssignment1/MainForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace KFurigayAssignment1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGame = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTurn = new System.Windows.Forms.Label();
+            this.pbBC = new System.Windows.Forms.PictureBox();
+            this.pbBR = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pbBL = new System.Windows.Forms.PictureBox();
+            this.pbMR = new System.Windows.Forms.PictureBox();
+            this.pbMC = new System.Windows.Forms.PictureBox();
+            this.pbML = new System.Windows.Forms.PictureBox();
+            this.pbTR = new System.Windows.Forms.PictureBox();
+            this.pbTC = new System.Windows.Forms.PictureBox();
+            this.pbTL = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbML)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTR)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGame
+            // 
+            this.lblGame.AutoSize = true;
+            this.lblGame.BackColor = System.Drawing.Color.Moccasin;
+            this.lblGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGame.Location = new System.Drawing.Point(187, 29);
+            this.lblGame.Name = "lblGame";
+            this.lblGame.Size = new System.Drawing.Size(301, 37);
+            this.lblGame.TabIndex = 1;
+            this.lblGame.Text = "Tic-Tac-Toe Game";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Moccasin;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(287, 66);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(101, 15);
+            this.lblName.TabIndex = 10;
+            this.lblName.Text = "by: Kin Furigay";
+            // 
+            // lblTurn
+            // 
+            this.lblTurn.AutoSize = true;
+            this.lblTurn.BackColor = System.Drawing.Color.Moccasin;
+            this.lblTurn.Location = new System.Drawing.Point(286, 423);
+            this.lblTurn.Name = "lblTurn";
+            this.lblTurn.Size = new System.Drawing.Size(0, 20);
+            this.lblTurn.TabIndex = 11;
+            // 
+            // pbBC
+            // 
+            this.pbBC.BackColor = System.Drawing.Color.Moccasin;
+            this.pbBC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbBC.Location = new System.Drawing.Point(286, 310);
+            this.pbBC.Name = "pbBC";
+            this.pbBC.Size = new System.Drawing.Size(98, 99);
+            this.pbBC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBC.TabIndex = 8;
+            this.pbBC.TabStop = false;
+            // 
+            // pbBR
+            // 
+            this.pbBR.BackColor = System.Drawing.Color.Moccasin;
+            this.pbBR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbBR.Location = new System.Drawing.Point(390, 310);
+            this.pbBR.Name = "pbBR";
+            this.pbBR.Size = new System.Drawing.Size(98, 99);
+            this.pbBR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBR.TabIndex = 9;
+            this.pbBR.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox8.Location = new System.Drawing.Point(286, 310);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(98, 99);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pbBL
+            // 
+            this.pbBL.BackColor = System.Drawing.Color.Moccasin;
+            this.pbBL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbBL.Location = new System.Drawing.Point(185, 310);
+            this.pbBL.Name = "pbBL";
+            this.pbBL.Size = new System.Drawing.Size(98, 99);
+            this.pbBL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbBL.TabIndex = 7;
+            this.pbBL.TabStop = false;
+            // 
+            // pbMR
+            // 
+            this.pbMR.BackColor = System.Drawing.Color.Moccasin;
+            this.pbMR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbMR.Location = new System.Drawing.Point(390, 205);
+            this.pbMR.Name = "pbMR";
+            this.pbMR.Size = new System.Drawing.Size(98, 99);
+            this.pbMR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMR.TabIndex = 6;
+            this.pbMR.TabStop = false;
+            // 
+            // pbMC
+            // 
+            this.pbMC.BackColor = System.Drawing.Color.Moccasin;
+            this.pbMC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbMC.Location = new System.Drawing.Point(286, 205);
+            this.pbMC.Name = "pbMC";
+            this.pbMC.Size = new System.Drawing.Size(98, 99);
+            this.pbMC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbMC.TabIndex = 5;
+            this.pbMC.TabStop = false;
+            // 
+            // pbML
+            // 
+            this.pbML.BackColor = System.Drawing.Color.Moccasin;
+            this.pbML.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbML.Location = new System.Drawing.Point(185, 205);
+            this.pbML.Name = "pbML";
+            this.pbML.Size = new System.Drawing.Size(98, 99);
+            this.pbML.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbML.TabIndex = 4;
+            this.pbML.TabStop = false;
+            // 
+            // pbTR
+            // 
+            this.pbTR.BackColor = System.Drawing.Color.Moccasin;
+            this.pbTR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbTR.Location = new System.Drawing.Point(390, 100);
+            this.pbTR.Name = "pbTR";
+            this.pbTR.Size = new System.Drawing.Size(98, 99);
+            this.pbTR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTR.TabIndex = 3;
+            this.pbTR.TabStop = false;
+            // 
+            // pbTC
+            // 
+            this.pbTC.BackColor = System.Drawing.Color.Moccasin;
+            this.pbTC.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbTC.Location = new System.Drawing.Point(289, 100);
+            this.pbTC.Name = "pbTC";
+            this.pbTC.Size = new System.Drawing.Size(98, 99);
+            this.pbTC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTC.TabIndex = 2;
+            this.pbTC.TabStop = false;
+            // 
+            // pbTL
+            // 
+            this.pbTL.BackColor = System.Drawing.Color.Moccasin;
+            this.pbTL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbTL.Location = new System.Drawing.Point(185, 100);
+            this.pbTL.Name = "pbTL";
+            this.pbTL.Size = new System.Drawing.Size(98, 99);
+            this.pbTL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbTL.TabIndex = 0;
+            this.pbTL.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.BackgroundImage = global::KFurigayAssignment1.Properties.Resources.bg1;
+            this.ClientSize = new System.Drawing.Size(662, 514);
+            this.Controls.Add(this.lblTurn);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pbBC);
+            this.Controls.Add(this.pbBR);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pbBL);
+            this.Controls.Add(this.pbMR);
+            this.Controls.Add(this.pbMC);
+            this.Controls.Add(this.pbML);
+            this.Controls.Add(this.pbTR);
+            this.Controls.Add(this.pbTC);
+            this.Controls.Add(this.lblGame);
+            this.Controls.Add(this.pbTL);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbML)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTR)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTL)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbTL;
+        private System.Windows.Forms.Label lblGame;
+        private System.Windows.Forms.PictureBox pbTC;
+        private System.Windows.Forms.PictureBox pbTR;
+        private System.Windows.Forms.PictureBox pbML;
+        private System.Windows.Forms.PictureBox pbMR;
+        private System.Windows.Forms.PictureBox pbBL;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pbBR;
+        private System.Windows.Forms.PictureBox pbMC;
+        private System.Windows.Forms.PictureBox pbBC;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTurn;
+    }
+}
+
---
+++ KFurigayAssignment1/MainForm.cs
@@ -0,0 +1,132 @@
+﻿// MainForm.cs
+// Assignment 1 that is a tic-tac-toe game.
+// Revision History: $Kin Furigay, 2023.09.26: Created. -- >
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KFurigayAssignment1
+{
+    public partial class MainForm : Form
+    {
+        // Declares variables
+        bool turn = true; // true = X turn, false = O turn
+        int turnCount = 0; // to count the turns taken, to check out whether it’s a draw when turnCount = 9
+        PictureBox[] pbArray;
+        string[] boardState = new string[9];
+        public MainForm()
+        {
+            InitializeComponent();  
+            InitializeGame();
+        }
+
+        // Initializes or resets the game to its default state.
+        private void InitializeGame()
+        {
+            turn = true; // X starts
+            turnCount = 0;
+            boardState = new string[9]; // Resetting the boardState
+            if (pbArray == null)
+                pbArray = new PictureBox[] { pbTL, pbTC, pbTR, pbML, pbMC, pbMR, pbBL, pbBC, pbBR };
+
+            foreach (var pb in pbArray)
+            {
+                pb.Image = null;
+                pb.Enabled = true;
+                pb.Click -= Pb_Click; // Unsubscribe before subscribing to avoid multiple subscriptions
+                pb.Click += Pb_Click; // adding a common click event handler for all PictureBoxes
+            }
+            UpdateTurnLabel();
+        }
+
+        // Handles the Click event for all PictureBox controls.
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox pb = (PictureBox)sender; 
+            int index = Array.IndexOf(pbArray, pb);
+            if (turn) // If it's X's turn
+            {   
+                pb.Image = Properties.Resources.X;
+                boardState[index] = "X";
+            }
+            else // If it's O's turn
+            {
+                pb.Image = Properties.Resources.O;
+                boardState[index] = "O";
+            }
+
+            turn = !turn; // Switch turns
+            pb.Enabled = false; // Disable the clicked PictureBox
+            turnCount++;
+            CheckWinner();
+            UpdateTurnLabel();
+        }
+
+        // Updates the turn label to display which player's turn it is.
+        private void UpdateTurnLabel()
+        {
+            if (turn) // If it's X's turn
+                lblTurn.Text = "TURN: Player 1 (X)";
+            else // If it's O's turn
+                lblTurn.Text = "TURN: Player 2 (O)";
+        }
+
+        // Checks if there is a winner or a draw and displays a message accordingly.
+        private void CheckWinner()
+        {
+            string winnerPlayer = CheckRows() ?? CheckColumns() ?? CheckDiagonals();
+
+            if (winnerPlayer != null)
+            {
+                lblTurn.Text = "";
+                MessageBox.Show($"{winnerPlayer} Wins!", "Game Over");
+                InitializeGame();
+            }
+            else if (turnCount == 9)
+            {
+                lblTurn.Text = "";
+                MessageBox.Show("It's a draw!", "Game Result");
+                InitializeGame();
+            }
+        }
+
+        // Checks each row to see if there is a winner.
+        private string CheckRows()
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                if (boardState[i] != null && boardState[i] == boardState[i + 1] && boardState[i + 1] == boardState[i + 2])
+                    return boardState[i];
+            }
+            return null;
+        }
+
+        // Checks each column to see if there is a winner.
+        private string CheckColumns()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (boardState[i] != null && boardState[i] == boardState[i + 3] && boardState[i + 3] == boardState[i + 6])
+                    return boardState[i];
+            }
+            return null;
+        }
+
+        // Checks the diagonals to see if there is a winner.
+        private string CheckDiagonals()
+        {
+            if ((boardState[0] != null && boardState[0] == boardState[4] && boardState[4] == boardState[8]) ||
+                (boardState[2] != null && boardState[2] == boardState[4] && boardState[4] == boardState[6]))
+                return boardState[4];
+
+            return null;
+        }
+    }
+}
